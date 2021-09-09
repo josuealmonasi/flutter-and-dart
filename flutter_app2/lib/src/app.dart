94988532c1206f7cb575c3658df6ea0becd34ee3
Josue Almonasi <josue.almonasi@hotmail.com>
2021-09-09 13:50:42 -0500

    Added home page

---
+++ flutter_app2/lib/src/app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app2/src/pages/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        // title: 'Hello world',
+        // home: Scaffold(
+        //   appBar: AppBar(
+        //     title: Text('Material App Bar'),
+        //   ),
+        //   body: Center(
+        //     child: Container(
+        //       child: Text('Hello World'),
+        //     ),
+        //   ),
+        // ),
+        home: Center(child: HomePage() ),
+        title: 'Hola mundo',);
+  }
+}
