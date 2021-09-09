b1dd952f9fd0b2b049c113e78f1e1e4b6fb43c74
Josue Almonasi <josue.almonasi@hotmail.com>
2021-09-09 15:57:47 -0500

    Finished counter

--- flutter_app2/lib/src/app.dart
+++ flutter_app2/lib/src/app.dart
@@ -1,23 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app2/src/pages/home_page.dart';
+import 'package:flutter_app2/src/pages/contador_page.dart';
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // title: 'Hello world',
-      // home: Scaffold(
-      //   appBar: AppBar(
-      //     title: Text('Material App Bar'),
-      //   ),
-      //   body: Center(
-      //     child: Container(
-      //       child: Text('Hello World'),
-      //     ),
-      //   ),
-      // ),
-      home: Center(child: HomePage()),
-      title: 'Hola mundo',
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Center(child: ContadorPage()),
+        title: 'Hola mundo',
+      );
 }
--- flutter_app2/lib/src/pages/home_page.dart
+++ flutter_app2/lib/src/pages/home_page.dart
@@ -7,27 +7,50 @@ class HomePage extends StatelessWidget {
   final style = new TextStyle(fontSize: 25);
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Titulo'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Text(
-              'Número de clicks:',
-              style: style,
-            ),
-            Text(
-              '0',
-              style: style,
-            ),
-          ],
-          mainAxisAlignment: MainAxisAlignment.center,
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text('Titulo'),
+          centerTitle: true,
         ),
-      ),
-    );
-  }
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Text(
+                'Número de clicks:',
+                style: style,
+              ),
+              Text(
+                '0',
+                style: style,
+              ),
+            ],
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
+        ),
+        floatingActionButton: Row(
+          children: [
+            FloatingActionButton(
+                onPressed: () {
+                  print('object');
+                },
+                mini: false,
+                tooltip: "Click me!",
+                child: Icon(Icons.volunteer_activism)),
+            FloatingActionButton(
+                onPressed: () {
+                  print('object');
+                },
+                mini: false,
+                tooltip: "Click me!",
+                child: Icon(Icons.plumbing)),
+            FloatingActionButton(
+                onPressed: () {
+                  print('object');
+                },
+                mini: false,
+                tooltip: "Click me!",
+                child: Icon(Icons.volunteer_activism)),
+          ],
+        ),
+      );
 }
